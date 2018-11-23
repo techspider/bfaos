08eb142cb54ebdf9843d5490b06ff680e064eaa5
mr_chainman (techspider) <rvreman7@me.com>
2018-11-23 08:28:01 -0800

    Add configurator

---
+++ configurator_src/windows/BFAOS/BFAOS/MainUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BFAOS
+{
+    public partial class MainUI : Form
+    {
+        public MainUI()
+        {
+            InitializeComponent();
+        }
+
+        private void MainUI_Load(object sender, EventArgs e)
+        {
+            this.Icon = SystemIcons.Application;
+        }
+    }
+}
